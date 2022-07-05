4d6dbf7f0e98543aa679beb4866c99b51ea9b9f3
Gaider10 <72764892+Gaider10@users.noreply.github.com>
2022-07-05 21:28:36 +0300

    Update to 1.19

--- src/main/java/me/andrew/healthindicators/HealthIndicatorsMod.java
+++ src/main/java/me/andrew/healthindicators/HealthIndicatorsMod.java
@@ -5,7 +5,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 
 public class HealthIndicatorsMod implements ModInitializer {
     public static final String MOD_ID = "healthindicators";
@@ -25,7 +25,7 @@ public class HealthIndicatorsMod implements ModInitializer {
             while (keyBinding.wasPressed()) {
                 toggled = !toggled;
                 if(client.player != null) {
-                    client.player.sendMessage(new LiteralText((toggled ? "Enabled" : "Disabled") + " Health Indicators"), true);
+                    client.player.sendMessage(Text.literal((toggled ? "Enabled" : "Disabled") + " Health Indicators"), true);
                 }
             }
         });
