65c95d830dbb5ef9e9f910119bd9dfc520b619f5
Gaider10 <72764892+Gaider10@users.noreply.github.com>
2023-04-16 12:41:21 +0300

    1.1.0

--- src/main/java/me/andrew/healthindicators/HealthIndicatorsMod.java
+++ src/main/java/me/andrew/healthindicators/HealthIndicatorsMod.java
@@ -6,26 +6,64 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 
 public class HealthIndicatorsMod implements ModInitializer {
     public static final String MOD_ID = "healthindicators";
 
-    public static KeyBinding keyBinding;
-    public static boolean toggled = true;
+    public static final String CONFIG_FILE = "healthindicators.json";
+
+    public static final KeyBinding RENDERING_ENABLED_KEY_BINDING = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key." + MOD_ID + ".renderingEnabled",
+            InputUtil.UNKNOWN_KEY.getCode(),
+            "key.categories." + MOD_ID
+    ));
+    public static final KeyBinding HEART_STACKING_ENABLED_KEY_BINDING = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key." + MOD_ID + ".heartStackingEnabled",
+            InputUtil.UNKNOWN_KEY.getCode(),
+            "key.categories." + MOD_ID
+    ));
+    public static final KeyBinding INCREASE_HEART_OFFSET_KEY_BINDING = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key." + MOD_ID + ".increaseHeartOffset",
+            InputUtil.UNKNOWN_KEY.getCode(),
+            "key.categories." + MOD_ID
+    ));
+    public static final KeyBinding DECREASE_HEART_OFFSET_KEY_BINDING = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key." + MOD_ID + ".decreaseHeartOffset",
+            InputUtil.UNKNOWN_KEY.getCode(),
+            "key.categories." + MOD_ID
+    ));
 
     @Override
     public void onInitialize() {
-        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key." + MOD_ID + ".toggle",
-                InputUtil.UNKNOWN_KEY.getCode(),
-                "key.categories." + MOD_ID
-        ));
+        Config.load();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (keyBinding.wasPressed()) {
-                toggled = !toggled;
-                if(client.player != null) {
-                    client.player.sendMessage(new LiteralText((toggled ? "Enabled" : "Disabled") + " Health Indicators"), true);
+            while (RENDERING_ENABLED_KEY_BINDING.wasPressed()) {
+                Config.setRenderingEnabled(!Config.getRenderingEnabled());
+                if (client.player != null) {
+                    client.player.sendMessage(new LiteralText((Config.getRenderingEnabled() ? "Enabled" : "Disabled") + " Health Indicators"), true);
+                }
+            }
+
+            while (HEART_STACKING_ENABLED_KEY_BINDING.wasPressed()) {
+                Config.setHeartStackingEnabled(!Config.getHeartStackingEnabled());
+                if (client.player != null) {
+                    client.player.sendMessage(new LiteralText((Config.getHeartStackingEnabled() ? "Enabled" : "Disabled") + " Heart Stacking"), true);
+                }
+            }
+
+            while (INCREASE_HEART_OFFSET_KEY_BINDING.wasPressed()) {
+                Config.setHeartOffset(Config.getHeartOffset() + 1);
+                if (client.player != null) {
+                    client.player.sendMessage(new LiteralText("Set heart offset to " + Config.getHeartOffset()), true);
+                }
+            }
+
+            while (DECREASE_HEART_OFFSET_KEY_BINDING.wasPressed()) {
+                Config.setHeartOffset(Config.getHeartOffset() - 1);
+                if (client.player != null) {
+                    client.player.sendMessage(new LiteralText("Set heart offset to " + Config.getHeartOffset()), true);
                 }
             }
         });
